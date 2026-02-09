2d7ff0598d26719df2f7825a859f5c803dcb0597
Chen Yan Jui <ch.focke@gmail.com>
2026-02-09 17:27:53 +0800

    infra: update firewall rules

--- deploy/main.go
+++ deploy/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"encoding/base64"
-	"fmt"
-	"strings"
 
 	"github.com/dirien/pulumi-vultr/sdk/v2/go/vultr"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -25,15 +23,6 @@ func main() {
 		}
 		instanceLabel := cfg.Require("label")
 
-		// --- 2. Cloudflare IPv4 清單 (約 15 條) ---
-		cfIpv4List := []string{
-			"173.245.48.0/20", "103.21.244.0/22", "103.22.200.0/22",
-			"103.31.4.0/22", "141.101.64.0/18", "108.162.192.0/18",
-			"190.93.240.0/20", "188.114.96.0/20", "197.234.240.0/22",
-			"198.41.128.0/17", "162.158.0.0/15", "104.16.0.0/13",
-			"172.64.0.0/13", "131.0.72.0/22",
-		}
-
 		fwGroup, err := vultr.NewFirewallGroup(ctx, "cloudflare-secure-fw", &vultr.FirewallGroupArgs{
 			Description: pulumi.Sprintf("Cloudflare Protected for %s", instanceLabel),
 		})
@@ -43,21 +32,14 @@ func main() {
 
 		// --- 3. 核心優化：合併 Port 80 與 443 ---
 		// 使用 "80:443" 範圍格式，一條規則同時搞定 HTTP 與 HTTPS
-		for i, cidr := range cfIpv4List {
-			parts := strings.Split(cidr, "/")
-			subnet := parts[0]
-			var size int
-			fmt.Sscanf(parts[1], "%d", &size)
 
-			vultr.NewFirewallRule(ctx, fmt.Sprintf("cf-web-range-%d", i), &vultr.FirewallRuleArgs{
-				FirewallGroupId: fwGroup.ID(),
-				Protocol:        pulumi.String("tcp"),
-				IpType:          pulumi.String("v4"),
-				Subnet:          pulumi.String(subnet),
-				SubnetSize:      pulumi.Int(size),
-				Port:            pulumi.String("80:443"), // 合併關鍵點
-			})
-		}
+		vultr.NewFirewallRule(ctx, "cf-web-range", &vultr.FirewallRuleArgs{
+			FirewallGroupId: fwGroup.ID(),
+			Protocol:        pulumi.String("tcp"),
+			IpType:          pulumi.String("v4"),
+			Source:          pulumi.String("cloudflare"),
+			Port:            pulumi.String("80:443"), // 合併關鍵點
+		})
 
 		// --- 4. 啟動腳本 (修正 HOME 變數問題) ---
 		rawScript := `#!/bin/bash
