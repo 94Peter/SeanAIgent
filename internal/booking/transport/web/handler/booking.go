c29fecf871f669cbf60c69a37377ff8e79617815
Chen Yan Jui <ch.focke@gmail.com>
2026-02-24 17:11:40 +0800

    hotfix: checkin time condition fix

--- internal/booking/transport/web/handler/booking.go
+++ internal/booking/transport/web/handler/booking.go
@@ -690,7 +690,7 @@ func (api *bookingAPI) getCheckinPage(c *gin.Context) {
 					ErrorMessage: "無法載入簽到列表，請稍後再試。",
 				}
 			} else {
-				if checkinList.StartDate.After(queryTime) &&
+				if checkinList.EndDate.After(queryTime) &&
 					checkinList.StartDate.Sub(queryTime) > 10*time.Minute {
 					viewModel = &checkin.CheckinPageModel{
 						ErrorMessage: "該時段尚未到達",
